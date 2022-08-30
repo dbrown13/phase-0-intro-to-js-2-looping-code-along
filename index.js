95d0fe6163422fc1cae15725c29d3d0c91cc0b99
Deryn Brown <Deryn.2017@gmail.com>
2022-08-30 14:52:27 -0500

    commit tree 822e09a982ce26e99d5adf28161b3e3aed0faf82

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages
+  }
+
+  function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        countDown--
+        console.log(countDown);
+    }
+  }
